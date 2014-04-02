c4a3035cd1df9904a9ed2f2bf1732dfe4a02fb55
Zarktao <zarktao@Gmail.com>
2014-04-02 20:59:13 +0800

    时间胶囊完善
    1. 将时间胶囊的图片读取更改为读取缩略图，提升图片读取速度
    2. 将自定义的TitleBar更改为ActionBar，更适合Android4.0及以上版本系统且解决了时间选择不能滑动的问题
    3. 其他细节方面优化

--- src/com/nju/Flash/time_capsule/content/Photo.java
+++ src/com/nju/Flash/time_capsule/content/Photo.java
@@ -2,14 +2,19 @@ package com.nju.Flash.time_capsule.content;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 
 public class Photo {
 	private static Photo photo;
 	private static Uri photoUri;
 	private static String name;
 	private static Activity activity;
+    private static int screenWidth = -1;
+    private static int screenHeight = -1;
 
 	private Photo(Uri uri) {
 		Photo.photoUri = uri;
@@ -43,7 +48,7 @@ public class Photo {
 
 	public static Photo getInstance() {
 		if (photo == null) {
-			createPhoto(Uri.parse("/res/drawable/nophoto.jpg"));
+			createPhoto(Uri.parse("/res/drawable/defaultpic.jpg"));
 		}
 		return photo;
 	}
@@ -59,4 +64,40 @@ public class Photo {
 	public static String getName(){
 		return name;
 	}
+
+    public static Bitmap thumb() {
+        String photoPath = getPhotoPath();
+        getDisplayPX();
+        BitmapFactory.Options options = getSize(photoPath);
+        options.inSampleSize = options.outWidth / screenWidth;
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        int height = options.outHeight * screenWidth / options.outWidth;
+        options.outWidth = screenWidth;
+        options.outHeight = height;
+        options.inJustDecodeBounds = false;
+        System.out.println("Width = "+options.outWidth+" ;Height = "+options.outHeight);
+        return BitmapFactory.decodeFile(photoPath, options);
+    }
+
+    private static BitmapFactory.Options getSize(String path){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(path, options);//这里返回的bmp是null
+        return options;
+    }
+    private static void getDisplayPX(){
+        DisplayMetrics dm =activity.getResources().getDisplayMetrics();
+        screenWidth = /*px2dip*/(dm.widthPixels);
+        screenHeight = /*px2dip*/(dm.heightPixels);
+        System.out.println("Screen Width = "+screenWidth+" ;Screen Height = "+screenHeight);
+    }
+    private static String getPhotoPath() {
+        String[] filePathColumns = { MediaStore.Images.Media.DATA };
+        Cursor c = activity.getContentResolver().query(photoUri, filePathColumns, null, null, null);
+        c.moveToFirst();
+        int columnIndex = c.getColumnIndex(filePathColumns[0]);
+        String picturePath = c.getString(columnIndex);
+        return picturePath;
+    }
 }
